fefa33c6989cc46bd4adaa1c6802e8308568f93a
马以 <ant@192.168.10.3>
2022-11-13 22:57:24 +0800

    geek-jvm

---
+++ geek-jvm/Profiteer.java
@@ -0,0 +1,30 @@
+package com.ant;
+
+
+import java.util.Random;
+
+/**
+ * <p>
+ * 奸商
+ * </P>
+ *
+ * @author Ant
+ * @since 2022/10/26 3:46 下午
+ **/
+public class Profiteer extends Merchant {
+    @Override
+    public double afterDiscount(double price, Customer customer) {
+        if (customer.isVIP()) {                             // invokeinterface
+            return price * priceDiscrimination();           // invokestatic
+        } else {
+            return super.afterDiscount(price, customer);    // invokespecial
+        }
+    }
+
+    public static double priceDiscrimination() {
+        // 咱们的杀熟算法太粗暴了，应该将客户城市作为随机数生成器的种子。
+        return new Random()                                 // invokespecial
+                .nextDouble()                               // invokevirtual
+                + 0.8d;
+    }
+}
